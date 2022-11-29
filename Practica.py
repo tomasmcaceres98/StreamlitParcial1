2efce2d53a7ec6bebad73eaec2ab448dcf786f92
tomasmcaceres98 <114121967+tomasmcaceres98@users.noreply.github.com>
2022-11-28 19:13:34 -0600

    Update Practica.py

--- Practica.py
+++ Practica.py
@@ -31,10 +31,10 @@ dfDatos['Upload'] = pd.to_numeric(dfDatos['Upload'])
 dfNuevo = dfDatos.groupby(dfDatos["Fecha"])[["Download","Upload"]].mean()
 dfNuevo
 st.title("Velocidad promedio de bajada de internet")
-fig1=px.line(dfNuevo,x=dfNuevo.index,y="Download", title="Velocidad Promedio de Internet",markers=True)
+fig1=px.bar(dfNuevo,x=dfNuevo.index,y="Download", title="Velocidad Promedio de Internet")
 st.plotly_chart(fig1, use_container_width=True)
 st.title("Velocidad promedio de subida de internet")
-fig2=px.line(dfNuevo,x=dfNuevo.index,y="Upload", title="Velocidad Promedio de Internet",markers=True)
+fig2=px.bar(dfNuevo,x=dfNuevo.index,y="Upload", title="Velocidad Promedio de Internet")
 st.plotly_chart(fig2, use_container_width=True)
 
 
